4ee6d74ad28fd453e65ff42ca5751f06747abee0
Markus Steinbach <markus.steinbach@tum.de>
2022-01-31 11:22:43 +0100

    updated runtests.sh

--- sampler/bin/runtests.sh
+++ sampler/bin/runtests.sh
@@ -2,42 +2,42 @@
 
 # positive tests
 # without batching
-echo "Run postive test: should run script without batching"
-bash sample.sh -d sequence -n 100 -l 1.0 -u 2.0 -r
+printf "Run postive test: should run script without batching\n"
+bash sample.sh -d sequence -n 10 -l 0.1 -u 0.5 -r
 
 # with batching
-echo "Run postive test: should run script with batching"
-bash sample.sh -d sequence -n 100 -l 1.0 -u 2.0 -b 20 -w 60 -r
+printf "\nRun postive test: should run script with batching\n"
+bash sample.sh -d sequence -n 10 -l 0.1 -u 0.5 -b 5 -w 6 -r
 
 # with anomalies
-echo "Run postive test: should run script with anomalies"
-bash sample.sh -d sequence -n 100 -l 1.0 -u 2.0 -b 20 -w 60 -r -e 2 -m 2
+printf "\nRun postive test: should run script with anomalies\n"
+bash sample.sh -d sequence -n 10 -l 0.1 -u 0.5 -b 5 -w 6 -r -e 1 -m 2
 
 # negative tests
 # upper bound missing
-echo "Run negative test: should exit with 'App directory name (flag: -d), number of traces (flag: -n), lower bound (flag: -l), or upper bound (flag: -u) is missing.'"
-bash sample.sh -d sequence -n 100 -l 1.0
+printf "\nRun negative test: should exit with 'Upper bound (flag: -u) is missing.'\n"
+bash sample.sh -d sequence -n 10 -l 1.0
 
 # lower bound larger than upper bound
-echo "Run negative test: should exit with 'Lower bound is larger than upper bound.'"
-bash sample.sh -d sequence -n 100 -l 2.0 -u 1.0
+printf "\nRun negative test: should exit with 'Lower bound is larger than upper bound.'\n"
+bash sample.sh -d sequence -n 10 -l 2.0 -u 1.0
 
 # batch size missing
-echo "Run negative test: should exit with 'Either batch size (flag: -b) or pause duration (flag: -w) argument is missing.'"
-bash sample.sh -d sequence -n 100 -l 1.0 -u 2.0 -w 60
+printf "\nRun negative test: should exit with 'Either batch size (flag: -b) or pause duration (flag: -w) argument is missing.'\n"
+bash sample.sh -d sequence -n 10 -l 0.1 -u 0.5 -w 60
 
 # pause duration missing
-echo "Run negative test: should exit with 'Either batch size (flag: -b) or pause duration (flag: -w) argument is missing.'"
-bash sample.sh -d sequence -n 100 -l 1.0 -u 2.0 -b 20
+printf "\nRun negative test: should exit with 'Either batch size (flag: -b) or pause duration (flag: -w) argument is missing.'\n"
+bash sample.sh -d sequence -n 10 -l 0.1 -u 0.5 -b 20
 
 # batch size 0
-echo "Run negative test: should exit with 'Batch size (flag: -b) and pause duration (flag: -w) must be larger than 0.'"
-bash sample.sh -d sequence -n 100 -l 1.0 -u 2.0 -b 0 -w 60
+printf "\nRun negative test: should exit with 'Batch size (flag: -b) and pause duration (flag: -w) must be larger than 0.'\n"
+bash sample.sh -d sequence -n 10 -l 0.1 -u 0.5 -b 0 -w 60
 
 # pause duration 0
-echo "Run negative test: should exit with 'Batch size (flag: -b) and pause duration (flag: -w) must be larger than 0.'"
-bash sample.sh -d sequence -n 100 -l 1.0 -u 2.0 -b 20 -w 0
+printf "\nRun negative test: should exit with 'Batch size (flag: -b) and pause duration (flag: -w) must be larger than 0.'\n"
+bash sample.sh -d sequence -n 10 -l 0.1 -u 0.5 -b 20 -w 0
 
 # anomaly flags negative
-echo "Run negative test: should exit with 'Anomalie flags -e and -m must be non-negative.'"
-bash sample.sh -d sequence -n 100 -l 1.0 -u 2.0 -e -1 -m -1
+printf "\nRun negative test: should exit with 'Anomalie flags -e and -m must be non-negative.'\n"
+bash sample.sh -d sequence -n 10 -l 0.1 -u 0.5 -e -1 -m -1
